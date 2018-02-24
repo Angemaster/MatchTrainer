e1f3379b8b6d1b08105d6fe543f93c2069a13db7
Manuel Ramon Razuri Seminario <1412938@utp.edu.pe>
2018-02-24 17:24:27 -0500

    commit 3

--- src/com/matchtrainer/models/CustomerScore.java
+++ src/com/matchtrainer/models/CustomerScore.java
@@ -1,5 +1,51 @@
 package com.matchtrainer.models;
 
 public class CustomerScore {
+    private int id;
+    private int trainerId;
+    private int starnumber;
+    private String  commentary;
 
+    public CustomerScore(int id, int trainerId, int starnumber, String commentary) {
+        this.setId(id);
+        this.setTrainerId(trainerId);
+        this.setStarnumber(starnumber);
+        this.setCommentary(commentary);
+    }
+
+    public CustomerScore() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(int trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public int getStarnumber() {
+        return starnumber;
+    }
+
+    public void setStarnumber(int starnumber) {
+        this.starnumber = starnumber;
+    }
+
+    public String getCommentary() {
+        return commentary;
+    }
+
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
 }
