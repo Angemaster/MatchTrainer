70934775181510fd4ee30eb6196a21f9128c21e6
Manuel Ramon Razuri Seminario <1412938@utp.edu.pe>
2018-02-24 18:14:16 -0500

    commit 4

--- src/com/matchtrainer/models/PaymentType.java
+++ src/com/matchtrainer/models/PaymentType.java
@@ -1,5 +1,32 @@
 package com.matchtrainer.models;
 
 public class PaymentType {
+    private int id;
+    private String name;
 
+
+    public PaymentType(int id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public PaymentType() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
