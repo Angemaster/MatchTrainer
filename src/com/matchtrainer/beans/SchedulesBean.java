ddd4735f8262cf910c2bbe4a421b740f50121ff1
erazaa <1531616@utp.edu.pe>
2018-02-25 20:38:12 -0500

    schedule class and Beans

---
+++ src/com/matchtrainer/beans/SchedulesBean.java
@@ -0,0 +1,73 @@
+package com.matchtrainer.beans;
+import com.matchtrainer.models.MatchTrainerService;
+import com.matchtrainer.models.Schedule;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SchedulesBean implements Serializable {
+
+    private MatchTrainerService service;
+    private Schedule schedule;
+
+    public SchedulesBean() {
+        service = new MatchTrainerService();
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+
+    public int getId(){
+        return this.getSchedule().getId();
+    }
+
+    public void setId(int id){
+        this.getSchedule().setId(id);
+    }
+
+    public int getTrainerId(){
+        return this.getSchedule().getTrainer_id();
+    }
+
+    public void setTrainerId(int id){
+        this.getSchedule().setTrainer_id(id);
+    }
+
+    public String getWeekday(){
+        return this.getSchedule().getWeekday();
+    }
+
+    public void setWeekday(String weekday){
+        this.getSchedule().setWeekday(weekday);
+    }
+
+    public Date getStarttime(){
+        return this.getSchedule().getStarttime();
+    }
+
+    public void setStarttime(Date starttime){
+        this.getSchedule().setStarttime(starttime);
+    }
+
+    public Date getEndtime(){
+        return this.getSchedule().getEndtime();
+    }
+
+    public void setEndtime(Date endtime){
+        this.getSchedule().setEndtime(endtime);
+    }
+
+    public boolean getEnabled(){
+        return this.getSchedule().getEnabled();
+    }
+
+    public void setEnabled(boolean enabled){
+        this.getSchedule().setEnabled(enabled);
+    }
+}
