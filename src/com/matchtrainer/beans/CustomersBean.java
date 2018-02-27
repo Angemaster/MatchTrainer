8d087a830a675c6ef61ac2492e72eaa58affb04e
patty <1321138@utp.edu.pe>
2018-02-27 03:39:57 -0500

    politica

--- src/com/matchtrainer/beans/CustomersBean.java
+++ src/com/matchtrainer/beans/CustomersBean.java
@@ -121,19 +121,6 @@ public class CustomersBean implements Serializable{
         return "success";
     }
 
-    public String newCustomer1(){
-        return "success";
-    }
-
-    public String newCustomer2(){
-        return "success";
-    }
-    public String newCustomer3(){
-        return "success";
-    }
-    public String newCustomer4(){
-        return "success";
-    }
 
     public String createCustomer(){
         User user =usersBean.createUser();
